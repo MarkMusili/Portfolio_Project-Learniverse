7d91df7996b0673fc1c701997a30852657c064a7
Ubuntu <ubuntu@ip-172-31-35-34.eu-central-1.compute.internal>
2024-02-19 11:36:40 +0000

    New commit

--- api.py
+++ api.py
@@ -10,14 +10,14 @@ app = Flask(__name__)
 CORS(app)
 
 client = openai.Client(
-    api_key = os.environ['OPENAI_API_KEY']
+    api_key = environ['OPENAI_API_KEY']
 )
 
 @app.route('/chat', methods=['POST'])
 def chat():
     data = request.get_json()
     completion = client.chat.completions.create(
-        model=os.environ['OPENAI_MODEL_ID'],
+        model = environ['OPENAI_MODEL_ID'],
         messages=[
             {"role": "system", "content": "You are an"},
             {"role": "user", "content": data["prompt"]}
