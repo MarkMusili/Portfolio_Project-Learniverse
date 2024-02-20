e63b100195efc481418321c1f56cabe1f5cf5605
MarkMusili <musiliyrn@gmail.com>
2024-02-20 16:27:07 +0300

    Changes to chat api

--- Web_static/Chat/chat.js
+++ Web_static/Chat/chat.js
@@ -4,10 +4,10 @@ $(document).ready(function () {
 
         const Prompt = $(".input").val();
 
-        // if (!Prompt.trim()) {
-        //     alert("Please enter your prompt");
-        //     return;
-        // }
+        if (!Prompt.trim()) {
+            alert("Please enter your prompt");
+            return;
+        }
 
         // Clear the input field
         $(".input").val("");
@@ -15,7 +15,7 @@ $(document).ready(function () {
         ///show after successfull call
         $(".response").show();
 
-        fetch("http://127.0.0.1:5000/chat", {
+        fetch("http://52.59.213.161:8080/chat", {
             method: "POST",
             body: JSON.stringify({ prompt: Prompt }),
             headers: {
@@ -24,9 +24,9 @@ $(document).ready(function () {
         })
             .then((response) => response.json())
             .then((data) => {
-                // $("ai-response").text(data)
-                // console.log(data)
-                json_data = JSON.parse(data)
+                //change later
+                $(".ai-response").text(data)
+                console.log(data)
             })
             .catch((error) => {
                 console.error("Error:", error);
