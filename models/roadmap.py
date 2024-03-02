13645f965d2b46e007fe6233d7029f7e01e9e2c9
Ote Ian <oteian9@gmail.com>
2024-03-03 00:11:53 +0300

    Fix: Syntax error

--- models/roadmap.py
+++ models/roadmap.py
@@ -13,9 +13,3 @@ class Roadmap(BaseModel, Base):
     AdditionalInfo = Column(JSON, nullable=True)
     topic = relationship('Topic', cascade='all, delete-orphan', backref='roadmap')
     dashboard = relationship('Dashboard', cascade='all, delete-orphan', backref='roadmap')
-
-<<<<<<< HEAD
-     
-=======
-     
->>>>>>> ed5af42f6c310a68448afb9ac57fa2e99ec8e882
