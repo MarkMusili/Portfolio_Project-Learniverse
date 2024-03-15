ea269bf505c69d467a54a89fc3d5bf98c01f0337
OteIan <oteian9@gmail.com>
2024-03-15 19:08:41 +0300

    .

--- web_flask/static/dashboard.js
+++ web_flask/static/dashboard.js
@@ -80,54 +80,54 @@ $(document).ready(function() {
     displayCount();
 });
 
-$(document).ready(function () {
-    $(".user_input").submit(function (event) {
-        event.preventDefault();
+// $(document).ready(function () {
+//     $(".user_input").submit(function (event) {
+//         event.preventDefault();
 
-        const Prompt = $(".input").val();
+//         const Prompt = $(".input").val();
 
-        if (!Prompt.trim()) {
-            alert("Please enter your prompt");
-            return;
-        }
+//         if (!Prompt.trim()) {
+//             alert("Please enter your prompt");
+//             return;
+//         }
 
-        // Clear the input field
-        $(".input").val("");
+//         // Clear the input field
+//         $(".input").val("");
 
-        ///show after successfull call
-        $(".response").show();
+//         ///show after successfull call
+//         $(".response").show();
 
-        fetch("http://52.59.213.161:8080/chat", {
-            method: "POST",
-            body: JSON.stringify({ prompt: Prompt }),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                fetch("http://52.59.213.161:8080/create_roadmap", {
-                    method: "POST",
-                    body: JSON.stringify(data),
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                })
-                .then(response => response.json())
-                .then((data) => {
-                    console.log(response.status); 
-                    if (response.ok) {
-                        url = 'http://5' + data
-                        window.open(url, "_blank");
-                    }
-                })
-                .catch((error) => {
-                    console.error("Error:", error);
-                });
-            })
-            .catch((error) => {
-                console.error("Error:", error);
-            });
-        });
-});
+//         fetch("http://52.59.213.161:8080/chat", {
+//             method: "POST",
+//             body: JSON.stringify({ prompt: Prompt }),
+//             headers: {
+//                 "Content-Type": "application/json",
+//             },
+//         })
+//             .then((response) => response.json())
+//             .then((data) => {
+//                 console.log(data);
+//                 fetch("http://52.59.213.161:8080/create_roadmap", {
+//                     method: "POST",
+//                     body: JSON.stringify(data),
+//                     headers: {
+//                         "Content-Type": "application/json",
+//                     },
+//                 })
+//                 .then(response => response.json())
+//                 .then((data) => {
+//                     console.log(response.status); 
+//                     if (response.ok) {
+//                         url = 'http://5' + data
+//                         window.open(url, "_blank");
+//                     }
+//                 })
+//                 .catch((error) => {
+//                     console.error("Error:", error);
+//                 });
+//             })
+//             .catch((error) => {
+//                 console.error("Error:", error);
+//             });
+//         });
+// });
