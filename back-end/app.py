4ba5c842a2684344e5116118cd6b5f7aa31e0bd0
OteIan <oteian9@gmail.com>
2024-06-26 18:56:43 +0300

    Dotenv

--- back-end/app.py
+++ back-end/app.py
@@ -30,7 +30,8 @@ from flask import Flask, request, jsonify, abort, redirect
 from flask_cors import CORS
 import openai
 import json
-from os import environ
+from os import environ, getenv
+from dotenv import load_dotenv
 from models import storage
 from models.auth import Auth
 
@@ -39,7 +40,8 @@ CORS(app)
 AUTH = Auth()
 
 # Initialize OpenAI client
-# client = openai.Client(api_key=environ['OPENAI_API_KEY'])
+load_dotenv()
+client = openai.Client(api_key=getenv('OPENAI_API_KEY'))
 
 # Close the database connection at the end of each request
 @app.teardown_appcontext
@@ -57,21 +59,21 @@ def db_close(exception):
 #     return render_template('login.html')
 
 # Endpoint for generating chat responses based on prompts
-# @app.route('/chat', methods=['POST'])
-# def chat():
-#     """
-#     Generate chat responses based on user prompts using the OpenAI chat model.
-#     """
-#     data = request.get_json()
-#     completion = client.chat.completions.create(
-#         model=environ['OPENAI_MODEL_ID'],
-#         messages=[
-#             {"role": "system", "content": "Given the specific topic, generate a comprehensive learning roadmap in json format. This should include a title for the whole concept, an engaging introduction, a detailed organization of topics and subtopics, learning objectives for each, numerous external links tailored to learners' preferences, time-based milestones, and optional additional information like tips and project ideas. Ensure the roadmap is flexible and diverse to adapt to various learners' needs and goals."},
-#             {"role": "user", "content": data["prompt"]}
-#         ]
-#     )
-#     response = completion.choices[0].message.content
-#     return jsonify(response)
+@app.route('/chat', methods=['POST'])
+def chat():
+    """
+    Generate chat responses based on user prompts using the OpenAI chat model.
+    """
+    data = request.get_json()
+    completion = client.chat.completions.create(
+        model=getenv('OPENAI_MODEL_ID'),
+        messages=[
+            {"role": "system", "content": "Given the specific topic, generate a comprehensive learning roadmap in json format. This should include a title for the whole concept, an engaging introduction, a detailed organization of topics and subtopics, learning objectives for each, numerous external links tailored to learners' preferences, time-based milestones, and optional additional information like tips and project ideas. Ensure the roadmap is flexible and diverse to adapt to various learners' needs and goals."},
+            {"role": "user", "content": data["prompt"]}
+        ]
+    )
+    response = completion.choices[0].message.content
+    return jsonify(response)
 
 # Endpoint for rendering the dashboard page
 @app.route('/dashboard')
