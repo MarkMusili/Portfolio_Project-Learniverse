10722b10417a17d778b5e862ea103da127b026a5
OteIan <oteian9@gmail.com>
2024-06-28 13:11:44 +0300

    Fix: Order of methods

--- back-end/app.py
+++ back-end/app.py
@@ -58,171 +58,6 @@ def db_close(exception):
 #     """ """
 #     return render_template('login.html')
 
-# Endpoint for generating chat responses based on prompts
-@app.route('/chat', methods=['POST'])
-def chat():
-    """
-    Generate chat responses based on user prompts using the OpenAI chat model.
-    """
-    data = request.get_json()
-    completion = client.chat.completions.create(
-        model=getenv('OPENAI_MODEL_ID'),
-        messages=[
-            {"role": "system", "content": "Given the specific topic, generate a comprehensive learning roadmap in json format. This should include a title for the whole concept, an engaging introduction, a detailed organization of topics and subtopics, learning objectives for each, numerous external links tailored to learners' preferences, time-based milestones, and optional additional information like tips and project ideas. Ensure the roadmap is flexible and diverse to adapt to various learners' needs and goals."},
-            {"role": "user", "content": data["prompt"]}
-        ]
-    )
-    response = completion.choices[0].message.content
-    return jsonify(response)
-
-# Endpoint for rendering the dashboard page
-@app.route('/dashboard')
-def dashboard():
-    """
-    Render the dashboard page with a list of available roadmaps.
-    """
-    roadmap = storage.all("Roadmap").values()
-    data = sorted(roadmap, key=lambda k: k.created_at, reverse=True)
-    roadmaps = [roadmap.to_dict() for roadmap in data]
-    # return render_template('dashboard.html', data=data)
-    return jsonify(roadmaps)
-
-# Endpoint for viewing a specific roadmap
-@app.route('/roadmap/<roadmap_id>')
-def view_roadmap(roadmap_id):
-    """
-    Render the page for viewing a specific roadmap.
-    """
-    roadmap = storage.show("Roadmap", roadmap_id)
-    if not roadmap:
-        return jsonify({'error': 'Roadmap not found'}), 404
-    
-    topics = storage.fetch("Topic", roadmap.id)
-    objectives = []
-    resources = []
-
-    for i in topics.values():
-        o = storage.fetch("Objectives", i.id)
-        r = storage.fetch("Resources", i.id)
-        for obj in o.values():
-            objectives.append(obj)
-        for res in r.values():
-            resources.append(res)
-
-    # return render_template('roadmap.html',
-    #                        roadmap=roadmap,
-    #                        objectives=objectives,
-    #                        topics=sorted(topics.values(), key=lambda t: t.position),
-    #                        resources=resources
-    #                        )
-
-    return jsonify({
-        'roadmap': roadmap.to_dict(),
-        'objectives': [obj.to_dict() for obj in objectives],
-        'topics': [topic.to_dict() for topic in sorted(topics.values(), key=lambda t: t.position)],
-        'resources': [res.to_dict() for res in resources]
-    })
-
-# Endpoint for creating a new roadmap
-@app.route('/create_roadmap', methods=['POST'])
-def create_roadmap():
-    """
-    Create a new roadmap based on the provided JSON data.
-    """
-    from models import storage
-    from models.roadmap import Roadmap
-    from models.topics import Topic
-    from models.resources import Resources
-    from models.objectives import Objectives
-
-    user_id = request.json.get("user_id")
-    if not user_id:
-        abort(403, descrption="Missing User Id")
-
-    roadmap_response = request.json.get("roadmaps")
-    if not roadmap_response:
-        abort(403, description="Missing Roadmap")
-    
-    user = storage.show("User", user_id)
-    if not user:
-        abort(400, description=f"User id: {user_id} Not Found")
-    
-    # for data in roadmap_response:
-    roadmap_data = {
-        'user_id': user_id,
-        'title': roadmap_response['Title'],
-        'introduction': roadmap_response['Introduction'],
-        'AdditionalInfo': roadmap_response['AdditionalInfo'],
-        'planning': True,
-        'in_progress': False,
-        'completed': False
-    }
-    roadmap = Roadmap(**roadmap_data)
-    roadmap.save()
-    r_id = roadmap.id
-
-    position = 1
-    for topic_data in roadmap_response["Topics"]:
-        topic = Topic(
-            position=position,
-            roadmap_id=roadmap.id,
-            name=topic_data['TopicName'],
-            description=topic_data['Descriptions'],
-            milestones=topic_data['Milestones']
-        )
-        position += 1
-
-        for objective_text in topic_data['LearningObjectives']:
-            objective = Objectives(name=objective_text, topic_id=topic.id)
-            topic.objectives.append(objective)
-            objective.save()
-
-        for resource_link in topic_data['Resources']:
-            resource = Resources(link=resource_link, topic_id=topic.id)
-            topic.resources.append(resource)
-            resource.save()
-
-        roadmap.topic.append(topic)
-        topic.save()
-    
-    user.roadmaps.append(roadmap)
-    user.save()
-
-    return jsonify({'roadmap_id': r_id}), 201
-
-# Endpoint for updating the status of a roadmap
-@app.route('/update_roadmap_status/<roadmap_id>', methods=['PUT'])
-def update_roadmap_status(roadmap_id):
-    """
-    Update the status of a roadmap (planning, in_progress, or completed).
-    """
-    try:
-        roadmap = storage.show("Roadmap", roadmap_id)
-        if not roadmap:
-            abort(404, description="Roadmap Not Found")
-
-        new_status = request.json.get('new_status')
-        if not new_status:
-            abort(403, description="Invalid Request body")
-
-        if new_status == 'planning':
-            roadmap.planning = True
-            roadmap.in_progress = False
-            roadmap.completed = False
-        elif new_status == 'in_progress':
-            roadmap.in_progress = True
-            roadmap.planning = False
-            roadmap.completed = False
-        elif new_status == 'completed':
-            roadmap.completed = True
-            roadmap.planning = False
-            roadmap.in_progress = False
-
-        roadmap.save()
-        return jsonify({'message': f'Roadmap status ({new_status}) updated successfully'}), 200
-    except Exception as e:
-        return jsonify({'error': f'An error occurred: {str(e)}'}), 500
-    
 @app.route('/users', methods=["POST"], strict_slashes=False)
 def register_user() -> str:
     """
@@ -315,6 +150,168 @@ def update_password() -> str:
         return jsonify({"email": email, "message": "Password updated"}), 200
     except ValueError:
         abort(403)
+
+
+# Endpoint for generating chat responses based on prompts
+@app.route('/chat', methods=['POST'])
+def chat():
+    """
+    Generate chat responses based on user prompts using the OpenAI chat model.
+    """
+    data = request.get_json()
+    completion = client.chat.completions.create(
+        model=getenv('OPENAI_MODEL_ID'),
+        messages=[
+            {"role": "system", "content": "Given the specific topic, generate a comprehensive learning roadmap in json format. This should include a title for the whole concept, an engaging introduction, a detailed organization of topics and subtopics, learning objectives for each, numerous external links tailored to learners' preferences, time-based milestones, and optional additional information like tips and project ideas. Ensure the roadmap is flexible and diverse to adapt to various learners' needs and goals."},
+            {"role": "user", "content": data["prompt"]}
+        ]
+    )
+    response = completion.choices[0].message.content
+    return jsonify(response)
+
+# Endpoint for rendering the dashboard page
+@app.route('/dashboard')
+def dashboard():
+    """
+    Render the dashboard page with a list of available roadmaps.
+    """
+    roadmap = storage.all("Roadmap").values()
+    data = sorted(roadmap, key=lambda k: k.created_at, reverse=True)
+    roadmaps = [roadmap.to_dict() for roadmap in data]
+
+    return jsonify(roadmaps)
+
+# Endpoint for creating a new roadmap
+@app.route('/create_roadmap', methods=['POST'])
+def create_roadmap():
+    """
+    Create a new roadmap based on the provided JSON data.
+    """
+    from models import storage
+    from models.roadmap import Roadmap
+    from models.topics import Topic
+    from models.resources import Resources
+    from models.objectives import Objectives
+
+    user_id = request.json.get("user_id")
+    if not user_id:
+        abort(403, descrption="Missing User Id")
+
+    roadmap_response = request.json.get("Roadmap")
+    if not roadmap_response:
+        abort(403, description="Missing Roadmap")
+    
+    user = storage.show("User", user_id)
+    if not user:
+        abort(400, description=f"User id: {user_id} Not Found")
+    
+    # for data in roadmap_response:
+    roadmap_data = {
+        'user_id': user_id,
+        'title': roadmap_response['Title'],
+        'introduction': roadmap_response['Introduction'],
+        'AdditionalInfo': roadmap_response['AdditionalInfo'],
+        'planning': True,
+        'in_progress': False,
+        'completed': False
+    }
+    roadmap = Roadmap(**roadmap_data)
+    roadmap.save()
+    r_id = roadmap.id
+
+    position = 1
+    for topic_data in roadmap_response["Topics"]:
+        topic = Topic(
+            position=position,
+            roadmap_id=roadmap.id,
+            name=topic_data['TopicName'],
+            description=topic_data['Descriptions'],
+            milestones=topic_data['Milestones']
+        )
+        position += 1
+
+        for objective_text in topic_data['LearningObjectives']:
+            objective = Objectives(name=objective_text, topic_id=topic.id)
+            topic.objectives.append(objective)
+            objective.save()
+
+        for resource_link in topic_data['Resources']:
+            resource = Resources(link=resource_link, topic_id=topic.id)
+            topic.resources.append(resource)
+            resource.save()
+
+        roadmap.topic.append(topic)
+        topic.save()
+    
+    user.roadmaps.append(roadmap)
+    user.save()
+
+    return jsonify({'roadmap_id': r_id}), 201
+
+
+# Endpoint for viewing a specific roadmap
+@app.route('/roadmap/<roadmap_id>')
+def view_roadmap(roadmap_id):
+    """
+    Render the page for viewing a specific roadmap.
+    """
+    roadmap = storage.show("Roadmap", roadmap_id)
+    if not roadmap:
+        return jsonify({'error': 'Roadmap not found'}), 404
+    
+    topics = storage.fetch("Topic", roadmap.id)
+    objectives = []
+    resources = []
+
+    for i in topics.values():
+        o = storage.fetch("Objectives", i.id)
+        r = storage.fetch("Resources", i.id)
+        for obj in o.values():
+            objectives.append(obj)
+        for res in r.values():
+            resources.append(res)
+
+    return jsonify({
+        'roadmap': roadmap.to_dict(),
+        'objectives': [obj.to_dict() for obj in objectives],
+        'topics': [topic.to_dict() for topic in sorted(topics.values(), key=lambda t: t.position)],
+        'resources': [res.to_dict() for res in resources]
+    })
+
+
+# Endpoint for updating the status of a roadmap
+@app.route('/update_roadmap_status/<roadmap_id>', methods=['PUT'])
+def update_roadmap_status(roadmap_id):
+    """
+    Update the status of a roadmap (planning, in_progress, or completed).
+    """
+    try:
+        roadmap = storage.show("Roadmap", roadmap_id)
+        if not roadmap:
+            abort(404, description="Roadmap Not Found")
+
+        new_status = request.json.get('new_status')
+        if not new_status:
+            abort(403, description="Invalid Request body")
+
+        if new_status == 'planning':
+            roadmap.planning = True
+            roadmap.in_progress = False
+            roadmap.completed = False
+        elif new_status == 'in_progress':
+            roadmap.in_progress = True
+            roadmap.planning = False
+            roadmap.completed = False
+        elif new_status == 'completed':
+            roadmap.completed = True
+            roadmap.planning = False
+            roadmap.in_progress = False
+
+        roadmap.save()
+        return jsonify({'message': f'Roadmap status ({new_status}) updated successfully'}), 200
+    except Exception as e:
+        return jsonify({'error': f'An error occurred: {str(e)}'}), 500
+    
 
 
 # @app.route('/profile')
