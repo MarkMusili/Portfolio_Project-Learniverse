784f8660f006666845be00488de2ada15a6a78a0
OteIan <oteian9@gmail.com>
2024-07-10 19:56:30 +0300

    Remove: db_storage (Had a lot of unnecessary packages)

--- back-end/models/__init__.py
+++ back-end/models/__init__.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 """This module instantiates an object of class based on the storage type"""
 from dotenv import load_dotenv
-from os import getenv
+from models.engine.VercelDB_storage import VercelDBStorage
 
 load_dotenv()
 
-if getenv("RDMP_TYPE_STORAGE") == "db":
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-else:
-    from models.engine.VercelDB_storage import VercelDBStorage
-    storage = VercelDBStorage()
-
+storage = VercelDBStorage()
 storage.reload()
