c3feb52ff9eb279225093d2daa449c6977d692ea
OteIan <oteian9@gmail.com>
2025-02-11 19:44:58 +0300

    Small efficiency change to decorators

--- back-end/auth/decorators.py
+++ back-end/auth/decorators.py
@@ -6,7 +6,6 @@ from functools import wraps
 from flask import request, abort, g
 from models import storage
 from sqlalchemy.orm.exc import NoResultFound
-from utils.request_utils import get_request_data
 
 
 def login_required(f):
@@ -15,7 +14,7 @@ def login_required(f):
     """
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        session_id = get_request_data(["session_id"]).get("session_id")
+        session_id = request.json.get("session_id")
         if not session_id:
             abort(401, description="Unauthorized")
 
